0b9f47151dc5a8ce02b5e4c95784dd8ffb048be1
desmondmccabe <78668579+desmondmccabe@users.noreply.github.com>
2022-03-12 18:40:32 +0000

    Update LoginServlet.java
    Added in basic functionality initially.
    Checking  for username 'admin' and password 'admin123'
    or
    username 'user' and password 'user123'
    
    anything else returns to login.html page

--- code/src/main/java/LoginServlet.java
+++ code/src/main/java/LoginServlet.java
@@ -3,12 +3,14 @@
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
- * Servlet implementation class HelloServlet
+ * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -25,8 +27,33 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    //System.out.println("Hello from Login Servlet");
+	    String username = request.getParameter("uname");
+	    String password = request.getParameter("psw");
+	    
+	    // get response writer
+	    response.setContentType("text/html"); 
+	    //object to print out  dynamic html contents. 
+	    PrintWriter writer = response.getWriter();
+	    if((username.equals("admin")) && (password.equals("admin123")))
+	      {
+		writer.print("<H4 style='font-size:12px'>");
+		writer.print("Hello, "+username);
+		writer.print("</H4>");
+	        request.getRequestDispatcher("/index.jsp").include(request, response);//request dispacher
+	      }
+	    else if((username.equals("user")) && (password.equals("user123")))
+	      {
+	         writer.print("<H4 style='font-size:12px'>");
+		 writer.print("Hello, "+username);
+		 writer.print("</H4>");
+	         request.getRequestDispatcher("/index.jsp").include(request, response);//request dispacher
+	      }
+	    else {
+	    	writer.print("<H2 style='color:red;font-style:bold;text-align:center'>Sorry, username or password error!, please try again</H2>");
+		request.getRequestDispatcher("/login.html").include(request, response); //request dispacher
+	    }
+	    writer.close();
 	}
 
 	/**
