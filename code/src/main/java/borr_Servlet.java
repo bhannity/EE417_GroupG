774052e1bee36b57345dbb5c9edbe111923d3e98
desmondmccabe <78668579+desmondmccabe@users.noreply.github.com>
2022-03-18 23:44:48 +0000

    Add files via upload
    Zeros the book stock in the database (as only 1 in stock) if borrow button is clicked.
    Return to ViewItems.jsp to see Availability = '0'

---
+++ code/src/main/java/borr_Servlet.java
@@ -0,0 +1,90 @@
+import java.sql.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class borr_Servlet
+ */
+@WebServlet("/borr_Servlet")
+public class borr_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public borr_Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String formVal = request.getParameter("name");
+		//String radio = request.getParameter("radio_act");
+		System.out.println("\nID is   = "+ formVal);
+		Connection con = null;  //connection object
+		PreparedStatement stmt =null;
+        ResultSet rs = null;   //result set object for receiving data 
+        String JDBCUrl = "jdbc:mysql://localhost:3306/library"; //define the URL for your database connection. 
+        String username = "root"; //local database usernanme
+        String password = "root"; //local database password
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        try {
+            System.out.println("\nConnecting to the SSD Database......");
+            Class.forName("com.mysql.jdbc.Driver"); //load the database driver
+            con = DriverManager.getConnection(JDBCUrl, username, password); //create the connection
+        }
+        catch (Exception e) {
+            System.out.println("\nAn error has occurred during the connection phase!  This is most likely due to your CLASSPATH being set wrong and the"
+                    + "  classes unable to be found.  Otherwise the database itself may be down.  Try telneting to port 3306 and see if it is up!");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        try {
+   	     System.out.println("\nConnection Successful..... creating statement....");
+   	     stmt = con.prepareStatement("UPDATE library.BOOKS SET STKLVL = 0 WHERE TITLE = ?");
+   	     stmt.clearParameters();       // Clears any previous parameters
+		 stmt.setString(1, formVal);
+		 request.getRequestDispatcher("/Borrow.html").include(request, response);
+		 stmt.executeUpdate();}
+   	     
+         catch (SQLException e) {
+   	     System.out.println("\nAn error has occurred during the Statement/ResultSet phase.  Please check the syntax and study the Exception details!");
+               while (e != null) {
+   	         System.out.println(e.getMessage());
+                   e = e.getNextException();
+   	     }
+               System.exit(0);
+           }
+
+           finally {
+   	     try {    
+   	         if (rs != null) rs.close();
+   		 if (stmt != null) stmt.close();
+   		 if (con != null) con.close();
+   	     }
+   	     catch (Exception ex) {
+   	         System.out.println("An error occurred while closing down connection/statement"); 
+               }
+           }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
