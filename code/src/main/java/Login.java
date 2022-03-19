cad4056f75ed4b9f9e25f7a39e8e7228aac880db
desmondmccabe <78668579+desmondmccabe@users.noreply.github.com>
2022-03-19 10:05:21 +0000

    Add files via upload
    Updated to display an error if incorrect username or password and additional prompt to 'Sign Up' if incorrect

--- code/src/main/java/Login.java
+++ code/src/main/java/Login.java
@@ -1,6 +1,8 @@
 //package form.login;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,7 +17,12 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String username = request.getParameter("uname");
 		String pass = request.getParameter("psw");
-		 HttpSession session = request.getSession(); 
+		HttpSession session = request.getSession();
+		
+		// get response writer
+	    response.setContentType("text/html"); 
+	    //object to print out  dynamic html contents. 
+	    PrintWriter writer = response.getWriter();
 		 
 		 LoginDao dao = new LoginDao();
 		
@@ -25,13 +32,15 @@ public class Login extends HttpServlet {
 			response.sendRedirect("Home.html");
 			
 		} else {
-			
+						
 			request.setAttribute("error","Invalid Username or Password");
-			RequestDispatcher rd=request.getRequestDispatcher("login.html");            
-			rd.include(request, response);
+			//RequestDispatcher rd=request.getRequestDispatcher("/login.html");  
+			request.getRequestDispatcher("/login.html").include(request, response);
+			writer.print("<H2 style='color:blue;font-style:bold;text-align:center'>Sorry, username or password error!, please try again.<br>Or <a href=\"signUp.html\" class=\"ml-2\" style=\"color:red\"><u><b>Sign Up</b></u></a> if you  don't already have an account</H2>");
+			//rd.include(request, response);
 			
 		}
-		
+		writer.close();
 		
 	}
 
