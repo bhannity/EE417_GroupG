96c96533c5e8e0eea8417b0382dcb344ef1531e9
desmondmccabe <78668579+desmondmccabe@users.noreply.github.com>
2022-03-25 01:11:21 +0000

    Added attributes to the room table in the DB.

--- db/library_des.sql
+++ db/library_des.sql
@@ -94,7 +94,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (3,'The Odyssey','	Homer','9780140449112','The epic tale of Odysseus and his ten-year journey home after the Trojan War forms one of the earliest and greatest works of Western literature.','Novel',1,NULL,NULL),(4,'Don Quixote','Miguel de Cervantes','9780393045147','Don Quixote has become so entranced by reading romances of chivalry that he determines to become a knight errant and pursue bold adventures.','Novel',1,NULL,NULL),(5,'One Hundred Years of Solitude','Gabriel Garcia Marquez','9780060919658','One Hundred Years of Solitude tells the story of the rise and fall, birth and death of the mythical town of Macondo through the history of the Buendiá family','Novel',1,NULL,NULL),(6,'The Great Gatsby','IF. Scott Fitzgerald','9780743273565','The Story Of The Mysteriously Wealthy Jay Gatsby And His Love For The Beautiful Daisy Buchanan','Novel',1,NULL,NULL),(7,'Lolita','Vladimir Nabokov','9780141182537','The Story Of The Mysteriously Wealthy Jay Gatsby And His Love For The Beautiful Daisy Buchanan','Novel',1,NULL,NULL),(8,'Crime and Punishment','Fyodor Dostoyevsky','9780486415871','Supreme masterpiece recounts in feverish, an impoverished student tormented by his own thoughts after he brutally murders an old woman','Novel',1,NULL,NULL),(1,'Ulysses','James Joyce','9780140185584','All the action of Ulysses takes place in and immediately around Dublin on a single day (June 16, 1904).','Novel',1,'',NULL),(2,'In Search of Lost Time','Marcel Proust','9781841598970','Recollections of childhood and experiences into adulthood in the late 19th-century \nand early 20th-century high-society France, while reflecting on the loss of time and lack of meaning in the world.The novel began to take shape in 1909.','Novel',1,NULL,NULL);
+INSERT INTO `books` VALUES (3,'The Odyssey','	Homer','9780140449112','The epic tale of Odysseus and his ten-year journey home after the Trojan War forms one of the earliest and greatest works of Western literature.','Novel',0,'user1','2022-04-15'),(4,'Don Quixote','Miguel de Cervantes','9780393045147','Don Quixote has become so entranced by reading romances of chivalry that he determines to become a knight errant and pursue bold adventures.','Novel',1,NULL,NULL),(5,'One Hundred Years of Solitude','Gabriel Garcia Marquez','9780060919658','One Hundred Years of Solitude tells the story of the rise and fall, birth and death of the mythical town of Macondo through the history of the Buendiá family','Novel',1,NULL,NULL),(6,'The Great Gatsby','IF. Scott Fitzgerald','9780743273565','The Story Of The Mysteriously Wealthy Jay Gatsby And His Love For The Beautiful Daisy Buchanan','Novel',1,NULL,NULL),(7,'Lolita','Vladimir Nabokov','9780141182537','The Story Of The Mysteriously Wealthy Jay Gatsby And His Love For The Beautiful Daisy Buchanan','Novel',1,NULL,NULL),(8,'Crime and Punishment','Fyodor Dostoyevsky','9780486415871','Supreme masterpiece recounts in feverish, an impoverished student tormented by his own thoughts after he brutally murders an old woman','Novel',1,NULL,NULL),(1,'Ulysses','James Joyce','9780140185584','All the action of Ulysses takes place in and immediately around Dublin on a single day (June 16, 1904).','Novel',0,'user1','2022-04-15'),(2,'In Search of Lost Time','Marcel Proust','9781841598970','Recollections of childhood and experiences into adulthood in the late 19th-century \nand early 20th-century high-society France, while reflecting on the loss of time and lack of meaning in the world.The novel began to take shape in 1909.','Novel',1,NULL,NULL);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,6 +108,11 @@ DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `room_id` int NOT NULL AUTO_INCREMENT,
   `room_no` char(50) NOT NULL DEFAULT '',
+  `username` varchar(30) DEFAULT NULL,
+  `fullname` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `BookDate` varchar(30) DEFAULT NULL,
+  `TIMESLOT` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`room_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -118,7 +123,7 @@ CREATE TABLE `room` (
 
 LOCK TABLES `room` WRITE;
 /*!40000 ALTER TABLE `room` DISABLE KEYS */;
-INSERT INTO `room` VALUES (1,'A'),(2,'B'),(3,'C'),(4,'D'),(5,'E');
+INSERT INTO `room` VALUES (1,'A',NULL,NULL,NULL,NULL,NULL),(2,'B','user1','John','Jones@joners.com','26/03/2022','11:00 To 12:00'),(3,'C',NULL,NULL,NULL,NULL,NULL),(4,'D',NULL,NULL,NULL,NULL,NULL),(5,'E',NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `room` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +150,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'user1','username','user@yahoo.com','1234'),(2,'test','Tester McTest','testing@test.com','tester'),(3,'test2','Tester McTest2','desmondmccabe3@gmail.com','testingallnight'),(4,'test22','Tester McTest22','desmondmccabe3@gmail.com','testingallnight2'),(5,'test22','Tester McTest22','desmondmccabe3@gmail.com','testingallnight2'),(6,'Des','Desmond','desmondmccabe3@gmail.com','desmond123');
+INSERT INTO `user` VALUES (1,'user1','username','user1@yahoo.com','1234'),(2,'test','Tester McTest','testing@test.com','tester'),(3,'test2','Tester McTest2','desmondmccabe3@gmail.com','testingallnight'),(4,'test22','Tester McTest22','desmondmccabe3@gmail.com','testingallnight2'),(5,'test22','Tester McTest22','desmondmccabe3@gmail.com','testingallnight2'),(6,'Des','Desmond','desmondmccabe3@gmail.com','desmond123');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -158,4 +163,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-22 21:11:47
+-- Dump completed on 2022-03-25  1:10:22
