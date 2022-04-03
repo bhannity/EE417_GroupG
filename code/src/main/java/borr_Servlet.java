858f6a0f0e43622944a924bd8972dd887421c2d5
desmondmccabe <78668579+desmondmccabe@users.noreply.github.com>
2022-04-03 13:49:10 +0100

    Looks like was an old version there

--- code/src/main/java/borr_Servlet.java
+++ code/src/main/java/borr_Servlet.java
@@ -1,13 +1,18 @@
 import java.sql.*;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+//import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.util.Calendar;
+import java.util.Date; 
+import java.text.SimpleDateFormat; 
 
 /**
  * Servlet implementation class borr_Servlet
@@ -29,16 +34,25 @@ public class borr_Servlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String formVal = request.getParameter("name");
-		//String radio = request.getParameter("radio_act");
 		System.out.println("\nID is   = "+ formVal);
+		HttpSession session = request.getSession();   //get the session ID
 		Connection con = null;  //connection object
 		PreparedStatement stmt =null;
         ResultSet rs = null;   //result set object for receiving data 
         String JDBCUrl = Common.getDatabaseUrlAndName();
         String username = Common.getDatabaseUser();
         String password = Common.getDatabasePassword();
-        PrintWriter out = response.getWriter();
+       
+        String sessID = (String)session.getAttribute("username");  // save the session ID as a a String
         response.setContentType("text/html");
+        Calendar cal = Calendar.getInstance();   //create a calender object
+        cal.add(Calendar.DATE, +21);   //add 3 weeks to the current data
+        Date date = cal.getTime();     //save it as a Data object
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); //format the date for mySQL
+           
+        String dueDate = formatter.format(date); //save as a String to populate the Database
+        System.out.println(dueDate);
+        
         try {
             System.out.println("\nConnecting to the SSD Database......");
             Class.forName("com.mysql.jdbc.Driver"); //load the database driver
@@ -52,9 +66,11 @@ public class borr_Servlet extends HttpServlet {
         }
         try {
    	     System.out.println("\nConnection Successful..... creating statement....");
-   	     stmt = con.prepareStatement("UPDATE library.BOOKS SET STKLVL = 0 WHERE TITLE = ?");
+  	     stmt = con.prepareStatement("UPDATE library.BOOKS SET STKLVL = 0,borrowed_By = ?,DUE_DATE=? WHERE TITLE = ?");
    	     stmt.clearParameters();       // Clears any previous parameters
-		 stmt.setString(1, formVal);
+   	     stmt.setString(1, sessID);
+   	     stmt.setString(2, dueDate);
+		 stmt.setString(3, formVal);
 		 stmt.executeUpdate();
 		 request.getRequestDispatcher("/Borrow.jsp").include(request, response);
         }
